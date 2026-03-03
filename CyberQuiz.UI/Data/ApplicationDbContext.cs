bdea06f894c316114f1050c9961b9231fab69cd8
SimonBergstrand-hub <simon.bergstrand@edu.newton.se>
2026-03-03 17:38:19 +0100

    Revert "Added logic so first user created is now admin"
    This reverts commit 0a6f4378d79d9e0a3fd4e006083b817203ae4e78.

--- CyberQuiz.UI/Data/ApplicationDbContext.cs
+++ CyberQuiz.UI/Data/ApplicationDbContext.cs
@@ -3,11 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CyberQuiz.UI.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
     }
 }
